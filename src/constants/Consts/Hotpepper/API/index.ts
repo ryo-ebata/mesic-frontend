7eab70381fc9498bdcfac98b0e18bfcfeb5a47a9
ryo-ebata <r.ebata@hajimari.inc>
2022-10-25 19:25:09 +0900

    環境変数周りを変更

--- src/constants/Consts/Hotpepper/API/index.ts
+++ src/constants/Consts/Hotpepper/API/index.ts
@@ -1,7 +1,10 @@
 import { Config } from "../../../Types/Hotpepper";
 
-const HOTPEPPER_SEACH_QUERY: string = process.env.HOTPEPPER_SEACH_QUERY!;
-const HOTPEPPER_API_KEY: string = process.env.HOTPEPPER_API_KEY!;
+// const HOTPEPPER_SEACH_QUERY: string = process.env.HOTPEPPER_SEACH_QUERY!;
+// const HOTPEPPER_API_KEY: string = process.env.HOTPEPPER_API_KEY!;
+
+const HOTPEPPER_SEACH_QUERY: string = "https://webservice.recruit.co.jp/hotpepper/gourmet/v1/";
+const HOTPEPPER_API_KEY: string = "92997d822eb26031";
 
 const HOTPEPPER_SEACH_CONFIG: Config  = {
     headers: {
