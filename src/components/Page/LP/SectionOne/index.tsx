f1d860e5804b408f0e20ce918eb98bc8ce190a76
ryo-ebata <r.ebata@hajimari.inc>
2022-11-06 22:49:57 +0900

    ✨ Section Oneを追加

--- src/components/Page/LP/SectionOne/index.tsx
+++ src/components/Page/LP/SectionOne/index.tsx
@@ -1,74 +1,94 @@
-import { motion, AnimatePresence } from "framer-motion";
-import { Three } from "../../../Organisms/Three";
+import { motion } from "framer-motion";
+import Image from "next/image";
 
 export const LP = () => {
   return (
     <motion.div
       style={{
         width: "100vw",
-        backgroundColor: "#000000",
         height: "100vh",
         margin: "0",
-        position: "relative",
-        display: "flex",
-        justifyContent: "center",
         overflow: "hidden",
+        objectFit: "cover",
+        background:
+          "linear-gradient(90deg, #F95738 0%, #F95738 50%, #E7DB73 50%, #E7DB73 100%)",
+        zIndex: "-1",
+        display: "flex",
+        justifyContent: "space-around",
+      }}
+      initial={{
+        opacity: 0,
       }}
       animate={{
-        opacity: [0, 1],
+        opacity: 1,
       }}
       transition={{
         duration: 1,
-        time: [0, 1],
       }}
       exit={{
         opacity: 0,
       }}
     >
-      <Three />
       <div
         style={{
-          position: "absolute",
-          top: "50%",
-          marginTop: "-5em",
+          margin: "auto",
+          top: "0",
+          bottom: "0",
         }}
       >
-        <motion.div
+        <img
+          src={"/img/spaghetti.png"}
+          alt="ロゴ"
           style={{
-            borderRadius: "50%",
-            backgroundColor: "#FFEF6C",
+            width: "500px",
+            height: "500px",
+            margin: "0",
           }}
-          animate={{
-            x: ["-180px", "0px", "0px", "0px", "-180px"],
-            width: ["100px", "100px", "500px", "500px", "0px"],
-            height: ["100px", "100px", "100px", "100px", "100px"],
-            borderRadius: ["50%", "50%", "0%", "0%", "0%"],
-          }}
-          transition={{
-            duration: 3,
-            time: [1, 1, 1, 1],
+        />
+      </div>
+      <div
+        style={{
+          margin: "auto",
+          top: "0",
+          bottom: "0",
+        }}
+      >
+        <p
+          style={{
+            color: "#F95738",
+            fontSize: "32px",
+            fontWeight: "bold",
+            margin: "0",
           }}
         >
-          <motion.p
-            style={{
-              color: "#FFEF6C",
-              fontSize: "80px",
-              fontWeight: "bold",
-            }}
-            animate={{
-              x: ["0px", "0px", "0px"],
-              y: ["0px", "0px", "0px"],
-              fontSize: ["80px", "80px", "80px"],
-              opacity: [0, 0, 1],
-            }}
-            transition={{
-              duration: 4,
-              time: [1, 2, 1],
-            }}
-          >
-            MesiCuu
-          </motion.p>
-        </motion.div>
+          誰かとご飯行くなら？
+        </p>
+        <h1
+          style={{
+            fontSize: "104px",
+            color: "#F95738",
+            textShadow: "#35A7FF 2px 2px 2px",
+            margin: "0",
+          }}
+        >
+          MesiCuu!
+        </h1>
+        <button
+          style={{
+            width: "100%",
+            height: "100px",
+            borderRadius: "8px",
+            backgroundColor: "#F95738",
+            color: "#E7DB73",
+            fontWeight: "bold",
+            fontSize: "32px",
+            border: "none",
+            boxShadow: "#35A7FF 2px 2px 2px",
+            margin: "0",
+          }}
+        >
+          今すぐ会員登録
+        </button>
       </div>
     </motion.div>
   );
