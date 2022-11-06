f448e7975d9c25bdca816574327807bf0e291ce0
ryo-ebata <r.ebata@hajimari.inc>
2022-11-06 13:19:28 +0900

    env

--- src/constants/Consts/Hotpepper/API/index.ts
+++ src/constants/Consts/Hotpepper/API/index.ts
@@ -1,21 +1,22 @@
 import { Config } from "../../../Types/Hotpepper";
 
-// const HOTPEPPER_SEACH_QUERY: string = process.env.HOTPEPPER_SEACH_QUERY!;
-// const HOTPEPPER_API_KEY: string = process.env.HOTPEPPER_API_KEY!;
+const HOTPEPPER_SEACH_QUERY: string = process.env.HOTPEPPER_SEACH_QUERY!;
+const HOTPEPPER_API_KEY: string = process.env.HOTPEPPER_API_KEY!;
 
-const HOTPEPPER_SEACH_QUERY: string = "https://webservice.recruit.co.jp/hotpepper/gourmet/v1/";
-const HOTPEPPER_API_KEY: string = "92997d822eb26031";
+// const HOTPEPPER_SEACH_QUERY: string = "https://webservice.recruit.co.jp/hotpepper/gourmet/v1/";
+// const HOTPEPPER_API_KEY: string = "92997d822eb26031";
 
-const HOTPEPPER_SEACH_CONFIG: Config  = {
-    headers: {
-        'Access-Control-Allow-Origin': '*',
-        'Access-Control-Allow-Headers': "X-Requested-With, Origin, X-Csrftoken, Content-Type, Accept"
-    },
-    params: {
-        key: HOTPEPPER_API_KEY,
-        keyword: 'w',
-        format: 'json'
-    },
-}
+const HOTPEPPER_SEACH_CONFIG: Config = {
+  headers: {
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Headers":
+      "X-Requested-With, Origin, X-Csrftoken, Content-Type, Accept",
+  },
+  params: {
+    key: HOTPEPPER_API_KEY,
+    keyword: "w",
+    format: "json",
+  },
+};
 
-export {HOTPEPPER_SEACH_QUERY, HOTPEPPER_API_KEY, HOTPEPPER_SEACH_CONFIG}
+export { HOTPEPPER_SEACH_QUERY, HOTPEPPER_API_KEY, HOTPEPPER_SEACH_CONFIG };
