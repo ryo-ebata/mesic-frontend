bd439d58df3f161bf0dc7202a9be1a68b42831eb
ryo-ebata <r.ebata@hajimari.inc>
2022-11-06 14:32:37 +0900

    Configの

--- src/constants/Consts/Hotpepper/Config/index.ts
+++ src/constants/Consts/Hotpepper/Config/index.ts
@@ -3,6 +3,11 @@ import { Config } from "../../../Types/Hotpepper";
 const HOTPEPPER_API_KEY: string = process.env.HOTPEPPER_API_KEY!;
 
 const HOTPEPPER_SEACH_CONFIG: Config = {
+  headers: {
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Headers":
+      "X-Requested-With, Origin, X-Csrftoken, Content-Type, Accept",
+  },
   params: {
     key: HOTPEPPER_API_KEY,
     keyword: "w",
