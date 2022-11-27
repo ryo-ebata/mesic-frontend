e473f3c0369c86d7bef6c6b0eb77747cdc532c58
ryo-ebata <r.ebata@hajimari.inc>
2022-11-27 15:39:08 +0900

    ✨ めちゃよくした

--- src/components/Atoms/Desk/index.tsx
+++ src/components/Atoms/Desk/index.tsx
@@ -5,11 +5,11 @@ export type Props = {
   size: number;
 };
 
-export const Steak: React.FC<Props> = ({ size }) => {
-  const { scene } = useGLTF("assets/steak.glb");
+export const Desk: React.FC<Props> = ({ size }) => {
+  const { scene } = useGLTF("assets/desk.glb");
 
   return (
-    <group dispose={null} position={[0, 8, 0]} rotation={[0.4, 1.5, 0]}>
+    <group dispose={null} position={[0, 3, 0]} rotation={[-0.5, 0, 0]}>
       <primitive scale={[size, size, size]} object={scene} />
     </group>
   );
